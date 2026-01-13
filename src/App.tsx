600589282f58afa21cb8373b8dd737314a59a201
Crazyxyz45 <parthbiradar28@gmail.com>
2026-01-13 20:09:20 +0530

    Fix: Add base path and switch to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import FarmerIntake from "./pages/FarmerIntake";
 import QualityGrading from "./pages/QualityGrading";
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/farmer" element={<FarmerIntake />} />
@@ -32,7 +32,7 @@ const App = () => (
           <Route path="/scan/:batchId" element={<ConsumerScan />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
